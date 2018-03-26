82df95017c4797b70d6c3c9e3d851354bd442365
Valentin Bora <valentin.bora@gmail.com>
2018-03-26 22:10:49 +0300

    Add PIVX

---
+++ coins/pivx.js
@@ -0,0 +1,13 @@
+var request = require('request');
+
+module.exports = (callback) => {
+    request('https://chainz.cryptoid.info/pivx/api.dws?q=circulating', (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            callback({
+                c: Number(body)
+            });
+        } else {
+            callback(null);
+        }
+    });
+};
