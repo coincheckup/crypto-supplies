fc8813315c718eed6b252d0667217e8ceb420b98
Valentin Bora <valentin.bora@gmail.com>
2018-03-26 22:10:37 +0300

    Add DigiByte (DGB)

---
+++ coins/digibyte.js
@@ -0,0 +1,14 @@
+var request = require('request');
+
+module.exports = (callback) => {
+    request('https://chainz.cryptoid.info/dgb/api.dws?q=circulating', (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            callback({
+                c: Number(body),
+                m: 21000000000
+            });
+        } else {
+            callback(null);
+        }
+    });
+};
