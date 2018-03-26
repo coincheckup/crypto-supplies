498c5f6083038c5d9b06d814d45903d41b6a73ff
Valentin Bora <valentin.bora@gmail.com>
2018-03-26 22:05:04 +0300

    Add Monero supply

---
+++ coins/monero.js
@@ -0,0 +1,15 @@
+var request = require('request');
+
+module.exports = (callback) => {
+    request('http://moneroblocks.info/api/get_stats/', (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            let supply = Number(JSON.parse(body).total_emission) * Math.pow(10, -12);
+
+            callback({
+                c: Math.round(supply * 100) / 100
+            });
+        } else {
+            callback(null);
+        }
+    });
+};
