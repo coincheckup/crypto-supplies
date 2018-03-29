e0303fa41bea06556db822ffef1b8956401b2e7e
kewagi <kewagi@icloud.com>
2018-03-29 10:46:28 +0200

    Added Bulwark

--- coins/bulwark.js
+++ coins/bulwark.js
@@ -1,5 +1,15 @@
 var request = require('request');
 
 module.exports = (callback) => {
-    callback(new Error('Not Implemented'));
+    request('http://explorer.bulwarkcrypto.com/api/supply', (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            let res = JSON.parse(body);
+            callback({
+              c: Number(res.c),
+              t: Number(res.t)
+            });
+        } else {
+            callback(new Error('Request error ' + response.statusCode));
+        }
+    });
 };
