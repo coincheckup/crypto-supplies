942a69c081894a4070faebd5f234dec42136c78c
Fabian Schuh <Fabian@chainsquad.com>
2018-05-14 11:48:28 +0200

    update bitshares

--- coins/bitshares.js
+++ coins/bitshares.js
@@ -1,5 +1,9 @@
 var request = require('request');
 
 module.exports = (callback) => {
-    callback(new Error('Not Implemented'));
+    callback({
+        c: 2624306979.45379,
+        t: 3600570502.10207,
+        m: 3600570502.10207
+    });
 };
