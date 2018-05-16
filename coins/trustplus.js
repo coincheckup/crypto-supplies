814bd923dd138c1b2c51ec094f0181b59f74aae0
Valentin Bora <valentin.bora@gmail.com>
2018-05-16 13:52:41 +0300

    Update trustplus.js with proper error handling

--- coins/trustplus.js
+++ coins/trustplus.js
@@ -1,13 +1,13 @@
 var request = require('request');
 
-+module.exports = (callback) => {
-+    request('https://chainz.cryptoid.info/trust/api.dws?q=circulating', (error, response, body) => {
-+        if (!error && response.statusCode == 200) {
-+            callback({
-+                c: Number(body)
-+            });
-+        } else {
-+            callback(null);
-+        }
-+    });
-+};
+module.exports = (callback) => {
+    request('https://chainz.cryptoid.info/trust/api.dws?q=circulating', (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            callback({
+                c: Number(body)
+            });
+        } else {
+            callback(new Error('Request error ' + response.statusCode));
+        }
+    });
+};
