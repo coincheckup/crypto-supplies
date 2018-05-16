483e9a6f615ef8a290c369731d06930539156470
Valentin Bora <valentin.bora@gmail.com>
2018-05-16 13:39:39 +0300

    Upgrade the script runner with yargs based arguments, pretty printing, better error handling

--- README.md
+++ README.md
@@ -14,25 +14,79 @@ As such, please help out by submitting a pull request with your favorite coin(s)
 
 Please submit ATOMIC EDITS, one pull request per one coin, do not mix them.
 
+# Payload structure
+
+Implemented coins should return an object via `callback()` with the following keys:
+
+- `c` representing the Circulating Supply
+- `t` representing the Total Supply
+- `m` representing the Max Supply
+
 # How to run all coins available
 
 1. Clone or download the repository
 2. Run `npm install`
-3. Run `node supplies.js`
+3. Run `node supplies.js get-all`
 
 # How to run an individual coin
 
 1. Clone or download the repository
 2. Run `npm install`
-3. Run `node supplies.js bitcoin.js`
+3. Run `node supplies.js get bitcoin`
 
-# Sample output
+# Caching
+
+This package uses a file-based cache with a default time to live (TTL) of 10 minutes.
+
+You can optionally pass an argument to override the TTL via `--cacheTTL 10` for 10 seconds for example.
+
+# Optional flags
+
+`--help` displays available commands and flags
+`--pretty` a boolean flag with no arguments, pretty prints output
+`--only-implemented` a boolean flag with no arguments, only displays implemented coins
+`--cacheTTL 10`  an integer flag with one argument, the number of seconds to cache responses
+
+# Sample basic output
 
 ```
-cardano.js { c: 31112484646, t: 31112484646, m: 45000000000 }
-neo.js { c: 65000000, t: 100000000, m: 100000000 }
-litecoin.js { c: 55805983.23335135, t: 55805983.23335135, m: 84000000 }
-bitcoin.js { c: 16937825, t: 21000000 }
-ripple.js { c: 99992401961.25706, t: 99992434971, m: 100000000000 }
-ethereum.js { c: 98424406.45 }
+> ./supplies.js get-all --only-implemented
+
+{"c":31112484646,"t":31112484646,"m":45000000000,"id":"cardano"}
+{"c":65000000,"t":100000000,"m":100000000,"id":"neo"}
+{"c":56560358.23095151,"t":56560358.23095151,"m":84000000,"id":"litecoin"}
+{"c":8073689.96392729,"t":8073689.96392729,"m":18900000,"id":"dash"}
+{"c":35017498.50875262,"id":"pivx"}
+{"c":98857486.20226616,"id":"stratis"}
+{"c":10258708190.732668,"m":21000000000,"id":"digibyte"}
+{"c":16036035.28,"id":"monero"}
+{"c":99992204718.08847,"t":99992434971,"m":100000000000,"id":"ripple"}
+{"c":99461419.46,"id":"ethereum"}
+{"c":7583686.03269673,"t":7583686.03269673,"id":"bulwark"}
+```
+
+# Sample pretty output
+
+```
+> ./supplies.js get-all --only-implemented --pretty
+
+cardano
+|-- Circulating: 31112484646
+|-- Total: 31112484646
+|-- Max: 45000000000
+
+neo
+|-- Circulating: 65000000
+|-- Total: 100000000
+|-- Max: 100000000
+
+bitcoin
+|-- Circulating: 17033525
+|-- Total: 21000000
+|-- Max: N/A
+
+litecoin
+|-- Circulating: 56560408.23095151
+|-- Total: 56560408.23095151
+|-- Max: 84000000
 ```
--- supplies.js
+++ supplies.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const {NodeVM} = require('vm2');
 const vm = new NodeVM({
     console: 'inherit',
@@ -9,35 +11,110 @@ const vm = new NodeVM({
     timeout: 1000
 });
 
+const yargs = require('yargs');
 const fs = require('fs');
 const chalk = require('chalk');
+const Cacheman = require('cacheman-file');
 
-const getSupplies = (file) => {
-    fs.readFile(file, 'utf8', (err, script) => {
-        try {
-            let res = vm.run(script, 'supplies.js');
+var cacheTTL = 600;
 
-            res((response) => {
-                if (response instanceof Error) {
-                    console.log(file, chalk.red(response.message));
-                } else {
-                    console.log(file, chalk.green(JSON.stringify(response)));
+const cache = new Cacheman({ ttl: cacheTTL });
+
+const printSupplies = (id, supplies, opts) => {
+    if (opts.pretty) {
+        if (supplies instanceof Error) {
+            console.log(id, chalk.red(supplies.message));
+        } else {
+            console.log(chalk.green(id));
+            console.log('|-- Circulating:', typeof supplies.c !== 'undefined' ? supplies.c : 'N/A');
+            console.log('|-- Total:', typeof supplies.t !== 'undefined' ? supplies.t : 'N/A');
+            console.log('|-- Max:', typeof supplies.m !== 'undefined' ? supplies.m : 'N/A');
+            console.log('');
+        }
+    } else {
+        if (supplies instanceof Error) {
+            let payload = {
+                error: supplies.message
+            };
+
+            if (typeof opts.includeIds !== 'undefined' && opts.includeIds) {
+                payload.id = id;
+            }
+
+            console.log(JSON.stringify(payload));
+        } else {
+            if (typeof opts.includeIds !== 'undefined' && opts.includeIds) {
+                supplies.id = id;
+            }
+
+            console.log(JSON.stringify(supplies));
+        }
+    }
+};
+
+const getSupplies = (id, opts) => {
+    var file = './coins/' + id + '.js';
+
+    cache.get(id, (err, value) => {
+        if (err === null && value !== null) {
+            printSupplies(id, value, opts);
+        } else {
+            fs.readFile(file, 'utf8', (err, script) => {
+                try {
+                    let res = vm.run(script, 'supplies.js');
+
+                    res((response) => {
+                        if (!(response instanceof Error)) {
+                            cache.set(id, response, opts.cacheTTL);
+                        }
+
+                        if (opts.onlyImplemented && response instanceof Error && response.message === 'Not Implemented') {
+                            return;
+                        }
+
+                        printSupplies(id, response, opts);
+                    });
+                } catch (e) {
+                    printSupplies(id, new Error(e.message), opts);
                 }
             });
-        } catch (e) {
-            console.log(file, chalk.red(e.message));
         }
     });
 };
 
-if (process.argv.length > 2) {
-    const file = './coins/' + process.argv[2];
+yargs.usage('$0 <cmd> [args]')
+    .command('get [id]', 'Get supplies for a specific coin id', (yargs) => {
+        yargs.positional('id', {
+            type: 'string',
+            describe: 'the coin id to get supplies for'
+        })
+    }, function (argv) {
+        getSupplies(argv.id, argv);
+    })
+    .command('get-all', 'Get supplies for all known coins', (yargs) => {
+    }, function (argv) {
+        argv.includeIds = true;
 
-    getSupplies(file);
-} else {
-    fs.readdir('./coins', (err, files) => {
-        files.forEach(file => {
-            getSupplies('./coins/' + file);
+        fs.readdir('./coins', (err, files) => {
+            files.forEach(file => {
+                getSupplies(file.replace('.js', ''), argv);
+            });
         });
-    });
-}
+    })
+    .option('pretty', {
+        default: false,
+        describe: 'pretty print output',
+        type: 'boolean'
+    })
+    .option('only-implemented', {
+        default: false,
+        describe: 'only display implemented coins',
+        type: 'boolean'
+    })
+    .option('cacheTTL', {
+        default: cacheTTL,
+        describe: 'how long to cache results, in seconds',
+        type: 'integer'
+    })
+    .help()
+    .argv;
