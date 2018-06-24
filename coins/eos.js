354d321acdf0ccacb0c7653b3c155c96951ac728
Valentin Bora <valentin.bora@gmail.com>
2018-06-24 20:21:45 +0300

    EOS script styling indentation update

--- coins/eos.js
+++ coins/eos.js
@@ -7,21 +7,21 @@
 var request = require('request');
 
 module.exports = (callback) => {
-request('http://api.ethplorer.io/getTokenInfo/0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0?apiKey=freekey', (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-        body = JSON.parse(body);
+    request('http://api.ethplorer.io/getTokenInfo/0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0?apiKey=freekey', (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            body = JSON.parse(body);
 
-        var resp = {
-            t: Number(body.totalSupply) * Math.pow(10, -18)
-        };
+            var resp = {
+                t: Number(body.totalSupply) * Math.pow(10, -18)
+            };
 
-        if (typeof body.price !== 'undefined' && typeof body.price.availableSupply !== 'undefined') {
-            resp.c = Number(body.price.availableSupply);
+            if (typeof body.price !== 'undefined' && typeof body.price.availableSupply !== 'undefined') {
+                resp.c = Number(body.price.availableSupply);
+            }
+
+            callback(resp);
+        } else {
+            callback(new Error('Request error ' + response.statusCode));
         }
-
-        callback(resp);
-    } else {
-        callback(new Error('Request error ' + response.statusCode));
-    }
-});
+    });
 };
