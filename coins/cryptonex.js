3094a2a9e6fda7dc055d6fe791e27f2218b7c8f0
Valentin Bora <valentin.bora@gmail.com>
2018-06-24 20:21:26 +0300

    Add dynamic implementation for Cryptonex (CNX)

--- coins/cryptonex.js
+++ coins/cryptonex.js
@@ -1,10 +1,23 @@
 /**
  * @title Cryptonex
  * @symbol CNX
- * @implementation Not Implemented
+ * @implementation Dynamic
  */
 var request = require('request');
 
 module.exports = (callback) => {
-    callback(new Error('Not Implemented'));
+    request({
+        uri: 'https://explorer.cryptonex.org/ext/getmoneysupply',
+        json: false
+    }, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            callback({
+                c: Number(body),
+                t: 106772751,
+                m: 210000000
+            })
+        } else {
+            callback(new Error('Request error ' + (typeof response !== 'undefined' ? response.statusCode : error.message)));
+        }
+    });
 };
