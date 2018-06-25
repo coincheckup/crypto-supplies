2a2e0275a00a8b7fb94dd0752584a7fdd153de82
Cristian Popa <n.cristian.p@gmail.com>
2018-06-25 23:28:32 +0300

    Updated data source in order to obtain dynamic circulating supply for Tron (TRX)

--- coins/tron.js
+++ coins/tron.js
@@ -1,19 +1,27 @@
 /**
  * @title TRON
  * @symbol TRX
+ * @ethContractAddr 0xf230b790e05390fc8295f4d3f60332c93bed42e2
  * @implementation Dynamic
  */
-var request = require('request');
+ var request = require('request');
 
-module.exports = (callback) => {
-    request('https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0xf230b790e05390fc8295f4d3f60332c93bed42e2', (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            callback({
-                c: 65748111645,
-                t: Number(JSON.parse(body).result) * Math.pow(10, -6)
-            })
-        } else {
-            callback(new Error('Request error ' + response.statusCode));
-        }
-    });
-};
+ module.exports = (callback) => {
+     request('http://api.ethplorer.io/getTokenInfo/0xf230b790e05390fc8295f4d3f60332c93bed42e2?apiKey=freekey', (error, response, body) => {
+         if (!error && response.statusCode == 200) {
+             body = JSON.parse(body);
+
+             var resp = {
+                 t: Number(body.totalSupply) * Math.pow(10, -6)
+             };
+
+             if (typeof body.price !== 'undefined' && typeof body.price.availableSupply !== 'undefined') {
+                 resp.c = Number(body.price.availableSupply);
+             }
+
+             callback(resp);
+         } else {
+             callback(new Error('Request error ' + response.statusCode));
+         }
+     });
+ };
