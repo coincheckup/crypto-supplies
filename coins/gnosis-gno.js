2d04ba6e383e982060de8418afe168b0ad20fd5b
Cristian Popa <n.cristian.p@gmail.com>
2018-06-25 23:23:46 +0300

    Updated data source in order to fetch dynamic circulating supply

--- coins/gnosis-gno.js
+++ coins/gnosis-gno.js
@@ -1,19 +1,27 @@
 /**
  * @title Gnosis
  * @symbol GNO
+ * @ethContractAddr 0x6810e776880c02933d47db1b9fc05908e5386b96
  * @implementation Dynamic
  */
-var request = require('request');
+ var request = require('request');
 
-module.exports = (callback) => {
-    request('https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0x6810e776880c02933d47db1b9fc05908e5386b96', (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            callback({
-                c: 1104590,
-                t: Number(JSON.parse(body).result) * Math.pow(10, -18)
-            })
-        } else {
-            callback(new Error('Request error ' + response.statusCode));
-        }
-    });
-};
+ module.exports = (callback) => {
+     request('http://api.ethplorer.io/getTokenInfo/0x6810e776880c02933d47db1b9fc05908e5386b96?apiKey=freekey', (error, response, body) => {
+         if (!error && response.statusCode == 200) {
+             body = JSON.parse(body);
+
+             var resp = {
+                 t: Number(body.totalSupply) * Math.pow(10, -18)
+             };
+
+             if (typeof body.price !== 'undefined' && typeof body.price.availableSupply !== 'undefined') {
+                 resp.c = Number(body.price.availableSupply);
+             }
+
+             callback(resp);
+         } else {
+             callback(new Error('Request error ' + response.statusCode));
+         }
+     });
+ };
