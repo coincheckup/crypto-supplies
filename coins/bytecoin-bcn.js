ae51237fdff2c3656ab10014d6fcdb7c3c73df72
Valentin Bora <valentin.bora@gmail.com>
2018-11-20 01:38:22 +0200

    Update ByteCoin (BCN) to handle API failures better

--- coins/bytecoin-bcn.js
+++ coins/bytecoin-bcn.js
@@ -10,7 +10,7 @@ module.exports = (callback, request) => {
         uri: 'https://chainradar.com/api/v1/bcn/status',
         json: true
     }, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode == 200 && typeof body.alreadyGeneratedCoins !== 'undefined') {
             callback({
                 c: Number(body.alreadyGeneratedCoins) * Math.pow(10, -8),
                 m: 184470000000
