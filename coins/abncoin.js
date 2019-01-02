0a0388061c9d8bc0508353d72f0e23cc7484dd39
Naor Ye <>
2019-01-02 20:02:24 +0200

    - Set meta data

--- coins/abncoin.js
+++ coins/abncoin.js
@@ -1,4 +1,9 @@
+/**
+ * @title Abncoin
+ * @symbol ABN
+ * @implementation Not Implemented
+ */
 
 module.exports = (callback, request) => {
-    callback(new Error('Not Implemented'));
+  callback(new Error("Not Implemented"));
 };
