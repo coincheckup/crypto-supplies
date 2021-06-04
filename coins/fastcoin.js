e72124c22189966119227029b5c8508b8d644f06
Fastcoin <76923827+fastcoin-project@users.noreply.github.com>
2021-06-04 02:12:24 -0600

    Update fastcoin.js

--- coins/fastcoin.js
+++ coins/fastcoin.js
@@ -6,5 +6,17 @@
  */
 
 module.exports = (callback, request) => {
-    callback(new Error('Not Implemented'));
+    request('https://chainz.cryptoid.info/fst/api.dws?q=circulating', (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            callback({
+                c: Number(body)
+            });
+        } else {
+            callback(new Error('Request error ' + typeof response !== 'undefined' ? response.statusCode : error));
+        }
+    });
 };
+
+//module.exports = (callback, request) => {
+//    callback(new Error('Not Implemented'));
+//};
